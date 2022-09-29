945536fe285f6cdb5d573b20aace396b0c9a1c06
lebed <lebed@DESKTOP-BKIICKL>
2022-09-29 13:52:29 +0300

    Add project files.

---
+++ 32.5.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+
+int main()
+{
+    std::ofstream file("film.json");
+    nlohmann::json myJSON = {
+        {"name",     "Legends of the Fall"},
+
+        {"country",  "United States of America"},
+
+        {"released",  "December 23, 1994 (US)"},
+
+        {"studio",  {"TriStar Pictures","The Bedford Falls Company"}},
+                     
+
+        {"screenplay by",   {"Susan Shilliday","William D. Wittliff"}},
+                      
+
+        {"director",    "Edward Zwick"},
+
+        {"producer", {"Marshall Herskovitz",
+                      "William D. Wittliff"}},
+
+        {"starring", {"Brad Pitt",
+                      "Anthony Hopkins",
+                      "Aidan Quinn",
+                      "Julia Ormond",
+                      "Henry Thomas"}},
+    };
+
+    file << myJSON;
+}
